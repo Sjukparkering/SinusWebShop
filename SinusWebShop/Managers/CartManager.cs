93703926784e009a717c12060f56df91f92cc10d
sjukparkering <danielsupina@hotmail.com>
2024-03-22 20:12:05 +0100

    Fixat cartpage

--- SinusWebShop/Managers/CartManager.cs
+++ SinusWebShop/Managers/CartManager.cs
@@ -1,16 +1,35 @@
-﻿using SinusWebShop.Models;
+﻿using System.Collections.Generic;
+using System.Linq;
+using SinusWebShop.Models;
 
 namespace SinusWebShop.Managers
 {
     public static class CartManager
     {
-            public static List<Product> Cart { get; set; } = new();
+        public static List<Product> Cart { get; set; } = new List<Product>();
 
-            public static double? GetTotalPrice()
-            {
-            return (double?)Cart.Sum(p => p.Price);
-            }
-      }
-    
+        public static double? GetTotalPrice()
+        {
+            return Cart.Sum(p => p.Price);
+        }
+
+        public static void AddToCart(Product product)
+        {
+            Cart.Add(product);
+        }
+
+        public static void RemoveFromCart(Product product)
+        {
+            Cart.Remove(product);
+        }
+    }
+    public class UserInformation
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string CardHolderName { get; set; }
+        public string CardNumber { get; set; }
+        public string CVC { get; set; }
+    }
 }
-
---
+++ SinusWebShop/Models/UserInformation.cs
@@ -0,0 +1,11 @@
+﻿namespace SinusWebShop.Models;
+/*{
+    public class UserInformation
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Address { get; set; }
+        public string? PaymentMethod { get; set; }
+    }
+}
+*/
